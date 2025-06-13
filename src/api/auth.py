9d7fed738c5a8292f80c45f7a611f28c9217d0b3
Dominic George <dominic.george@gmail.com>
2025-06-13 14:35:07 +1000

    breaking the single endpoint file to service, liek boarding-service, fligh-management-service, trip-management-service

--- src/api/auth.py
+++ src/api/auth.py
@@ -11,9 +11,9 @@ api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
 
 # List of valid API keys (for testing; in production, use a secure storage like environment variables)
 #API_KEYS = ["my-secret-key", "another-secret-key"]
-print(f"API_KEY_HEADER : {api_key_header}")
+
 API_KEY = os.getenv(api_key_secret_name)
-print(f"API_KEYS : {API_KEY}")
+
 async def get_api_key(api_key: str = Depends(api_key_header)):
     if not api_key or api_key != API_KEY:
         raise HTTPException(
