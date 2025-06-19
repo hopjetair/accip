8b578ae22c8d50936af5b95c7249a9bbc1f16f09
Dominic George <dominic.george@gmail.com>
2025-06-20 09:31:30 +1000

    fixed the password

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ nonprod = os.getenv("NONPROD", "True").lower() == "true"
 db_host = os.getenv("DB_HOST", "localhost")
 db_name = os.getenv("DB_NAME", "hopjetairline_db")
 db_user = os.getenv("DB_USER", "postgres")
-db_pass = os.getenv("DB_PASS", "Testing!@123")
+db_pass = os.getenv("DB_PASS", "SecurePass123!")
 db_port = os.getenv("DB_PORT", "5432")  # Aurora default port is 5432
 
 db_testname = os.getenv("DB_TESTNAME", "test_hopjetairline_db")
